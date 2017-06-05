ed96adb7d82517ed59f4882f02955220808ef8c9
kwichmann <kristian.wichmann@gmail.com>
2017-06-05 03:52:33 +0200

    Math fixes and improved commenting

--- docs/XOR_nn/matrix_math.js
+++ docs/XOR_nn/matrix_math.js
@@ -1,14 +1,21 @@
+// Data points
 const X = math.matrix([	[1, 1, 1, 1],
 						[0, 1, 0, 1],
 						[0, 0, 1, 1] ]);
 
+// Corresponding XOR results
 const Y = math.matrix([[0, 1, 1, 0]]);
 
+// Weight/bias matrices
 var Theta1, Theta2;
+
+// Activations, activations with added row of ones, hypothesis
 var A, A0, H;
 
+// Errors in the hidden and output layers respectively
 var Delta1, Delta2;
 
+// Derivatives of the weight matrices
 var derivatives1, derivatives2;
 
 // Randomizer helper function
@@ -22,7 +29,6 @@ function rand(r) {
 function init_weights(r) {
 	Theta1 = math.matrix([  [rand(r), rand(r), rand(r)],
 							[rand(r), rand(r), rand(r)] ]);
-
 	Theta2 = math.matrix([[rand(r), rand(r), rand(r)]]);
 }
 
@@ -40,25 +46,37 @@ function forward_propagate() {
 	A = sigma(math.multiply(Theta1, X));
 	
 	// Add row of ones - not the most beautiful way, I'm sure :-/
-	A0 = math.matrix([[1, 1, 1, 1], A.toArray()[0], A.toArray()[0]]);
+	A0 = math.matrix([[1, 1, 1, 1], A.toArray()[0], A.toArray()[1]]);
 
 	H = sigma(math.multiply(Theta2, A0));
 }
 
+// Backpropagation calculation
+
 function backpropagate() {
+
+	// Calculate output layer errors and derivatives
 	Delta2 = math.subtract(H, Y);
 	derivatives1 = math.multiply(Delta2, math.transpose(A0));
 
+	// Get Theta2 without bias and sigmoid derivaites
 	var theta2 = Theta2.subset(math.index(0, [1, 2]));
 	var sigma_derivative = math.dotMultiply(A, math.subtract(math.ones(2, 4), A));
 	
+	// Use to backpropagate to errors in hidden layer and calculate derivatives
 	Delta1 = math.dotMultiply(math.multiply(math.transpose(theta2), Delta2), sigma_derivative);
+	derivatives2 = math.multiply(Delta1, math.transpose(X));
 }
 
+// Update weights using gradient descent with learning rate alpha
+
 function gradient_descent(alpha) {
+
+	// Calculate derivatives
 	forward_propagate();
 	backpropagate();
 
+	// Update
 	Theta1 = math.subtract(Theta1, math.dotMultiply(alpha, derivatives1));
 	Theta2 = math.subtract(Theta2, math.dotMultiply(alpha, derivatives2));
 }
