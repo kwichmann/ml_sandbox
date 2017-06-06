f2e7b24fd232a54bf0684c44aabccf5ef6d0b9d9
kwichmann <kristian.wichmann@gmail.com>
2017-06-06 10:16:22 +0200

    Interactivity improved. Data points now shown

--- docs/XOR_nn/draw_prediction.js
+++ docs/XOR_nn/draw_prediction.js
@@ -1,11 +1,23 @@
 function draw_prediction() {
 	noStroke();
+	var sz = sz_slider.value();
 
-	for (var i = 0; i < 20; i++) {
-		for (var j = 0; j < 20; j++) {
-			var pred = predict(map(i, 0, 20, -0.5, 1.5), map(j, 0, 20, 1.5, -0.5));
+	for (var i = 0; i < 40; i++) {
+		for (var j = 0; j < 40; j++) {
+			var pred = predict(map(i, 0, 40, -sz, 1 + sz), map(j, 0, 40, 1 + sz, -sz));
 			fill(map(pred, 0, 1, 0, 255));
-			rect(map(i, 0, 20, 300, 500), map(j, 0, 20, 25, 225), 10, 10);
+			rect(map(i, 0, 40, 300, 500), map(j, 0, 40, 25, 225), 5, 5);
 		}
 	}
+
+	// Draw data points
+	stroke(200, 200, 255);
+
+	fill(255);
+	ellipse(map(0, -sz, 1 + sz, 300, 500), map(0, -sz, 1 + sz, 25, 225), 10, 10);
+	ellipse(map(1, -sz, 1 + sz, 300, 500), map(1, -sz, 1 + sz, 25, 225), 10, 10);
+	
+	fill(0);
+	ellipse(map(0, -sz, 1 + sz, 300, 500), map(1, -sz, 1 + sz, 25, 225), 10, 10);
+	ellipse(map(1, -sz, 1 + sz, 300, 500), map(0, -sz, 1 + sz, 25, 225), 10, 10);
 }
--- docs/XOR_nn/sketch.js
+++ docs/XOR_nn/sketch.js
@@ -1,15 +1,28 @@
+var lr_slider, sz_slider;
+
 function setup() {
-	createCanvas(525, 250)
-	init_weights(0.5);
+	var canvas = createCanvas(525, 250)
+	select("#canvas").child(canvas);
+
+	lr_slider = createSlider(0, 2, 1, 0.05);
+	select("#learning_rate").child(lr_slider);
+
+	sz_slider = createSlider(0.2, 5, 2, 0.1);
+	select("#prediction_size").child(sz_slider);
+
+	var reset_button = createButton("Reset");
+	reset_button.mousePressed(initialize);
+
+	initialize();
 }
 
 function draw() {
 	background(200);
 	draw_network();
 	draw_prediction();
-	gradient_descent(1);
+	gradient_descent(lr_slider.value());
 }
 
-function mouseClicked() {
+function initialize() {
 	init_weights(0.5);
 }
